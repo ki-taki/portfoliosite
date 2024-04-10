9bd84175172c234d43c3735d9344bbd615b14d06
ta-kit <kksw10@icloud.com>
2024-04-11 02:01:36 +0900

    first commit

---
+++ components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class="l-footer">
+        <p class="l-footer__txt">©2024 t-portfolio</p>
+    </footer>
+</template>
+
+<style scoped lang="scss">
+.l-footer {
+    padding: 2.5rem;
+    text-align: center;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useHead({
+    titleTemplate: (title) => {
+        return title ? ` T - ${title}` : ' T';
+    },
+})
+</script>
+
+<template>
+    <Header />
+    <slot></slot>
+    <Footer />
+    <Background />
+</template>
